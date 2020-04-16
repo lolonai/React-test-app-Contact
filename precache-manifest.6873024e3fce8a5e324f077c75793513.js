bb2e11d93e9adc078b6148f48125c40d286a4552
laurent naitza <lolonaitza@gmail.com>
2020-04-16 02:35:19 +0200

    Updates

---
+++ precache-manifest.6873024e3fce8a5e324f077c75793513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778b2e28d2f7cb726eb832002334ab6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2a73c17f8e06bf17bb7",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c2a73c17f8e06bf17bb7",
+    "url": "/static/js/2.27d2ab05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.27d2ab05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ad73b82b43e5337caa",
+    "url": "/static/js/main.9dd26f29.chunk.js"
+  },
+  {
+    "revision": "37b4a46b444c0e016027",
+    "url": "/static/js/runtime-main.1252e4e3.js"
+  }
+]);
